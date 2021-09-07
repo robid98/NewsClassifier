8da2973b141b4aa10395b38519d52f10c296a05d
Robi <89996135+robid98@users.noreply.github.com>
2021-09-07 09:07:55 +0300

    Update settings.py

--- django_news_classifier/settings.py
+++ django_news_classifier/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i9tc(u-)lvf0f9yc$7jy0g=9_!*@v*pb4vhv)-r71(3zf$6+7w'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
